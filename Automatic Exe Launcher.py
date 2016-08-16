6b70ce6fbba5753557fc35614acfb2e7209bd96e
Derek Santos <derekhalo@gmail.com>
2016-08-16 19:53:10 -0400

    First Commit

---
+++ Automatic Exe Launcher.py
@@ -0,0 +1,40 @@
+"""
+    By: Derek Santos
+    Explaination:
+        A python script to open applications whenever one needs them to be
+        opened. This helps when an application is expecting an update though 
+        it needs to be newly opened while I'm not currently at home,
+        or at my computer.
+
+"""
+
+from datetime import datetime
+import subprocess
+
+# Write path to .exe that you want
+application = r"C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe"
+expected = {'hour': 0, 'minute': 0, 'ampm': 'am'}
+
+#User input
+print('Uses 12 hour time...')
+expected['hour'] = int(input('What HOUR do you want?: '))
+expected['minute'] = int(input('What MINUTE do you want?: '))
+expected['ampm'] = str(input('AM or PM?: ')).lower()
+
+#goes from 24 to 12 timelines.
+if (expected['ampm'] == 'pm') and (expected['hour'] < 12):
+    expected['hour'] += 12
+
+print(expected['hour'])
+print(expected['minute'])
+print(expected['ampm'])
+while (True):
+    #when current time is > than now execute
+    if (datetime.now().hour == expected['hour']) and (datetime.now().minute == expected['minute']):
+
+        try:
+            subprocess.Popen([application])
+            break
+        except Exception as e:
+            print('Could not open application...\n', e)
+            break
---
+++ Readme.txt
@@ -0,0 +1,4 @@
+By: Derek Santos
+
+At a certain time, this script will launch the desired *.exe.
+A python script to open applications whenever I need them to be opened. This helps when I'm expecting an update for an application though it needs to be newly opened while I'm not currently at home, or at my computer.
